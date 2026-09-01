d9311ef8c413ba0cd5bb437cd236a624b0c3b441
miikaestefani6-eng <miikaestefani6@icloud.com>
2026-09-01 14:33:28 -0300

    feat: connect agenda route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Agenda from "./Agenda";
 import "./styles.css";
+import "./nexo-enhancements.css";
+
+function Router() {
+  const path = window.location.pathname;
+
+  if (path === "/agenda") {
+    return <Agenda />;
+  }
+
+  return <App />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
